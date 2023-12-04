72abcef09c318bc059462dd6065156bbd04f5f59
FantiGA <98534535+FantiGA@users.noreply.github.com>
2023-12-04 10:15:21 +0900

    Fix known security vulnerabilities

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: fantiga
+ * @Date: 2023-02-05 21:00:19
+ * @LastEditTime: 2023-09-02 23:44:46
+ * @LastEditors: fantiga
+ * @FilePath: /clinical-portal-demo-react-ts/babel.config.js
+ */
 /**
  * @Author: fantiga
  * @Date: 2022-12-24 18:01:25
